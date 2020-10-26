38ea4a9042c35f5dcbb65ed6daaa8dce3246326d
Firouzfar <firouzfar.sebastien@gmail.com>
2020-10-26 11:47:36 +0100

    Fin exo 5.9

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -13,4 +13,24 @@
 
     // your code here
 
+    document.getElementById("run").addEventListener("click", function(){
+    let tableau = []
+    // genérer des nombres random
+    for (let i=0; i<10;i++){
+        tableau.push(Math.floor(Math.random()*100+1));
+    }
+
+    //générer l'affichage dans la page web 
+    for (let j=1;j<11;j++){
+        document.getElementById("n-"+j).innerHTML=tableau[j-1];
+    } 
+
+    document.getElementById("min").innerHTML=Math.min(...tableau);
+    document.getElementById("max").innerHTML=Math.max(...tableau);
+    let sum = tableau.reduce(function(a, b) { return a + b; }, 0);
+    document.getElementById("sum").innerHTML=sum;
+    document.getElementById("average").innerHTML=sum/tableau.length;
+
+    })
+
 })();
