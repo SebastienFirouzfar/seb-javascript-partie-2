2ceef4017ae7e9915e7208693ee0c7e3ad98b667
Firouzfar <firouzfar.sebastien@gmail.com>
2020-10-29 15:23:55 +0100

    Fin exo 8.7

--- 08-dom/07-generate-table-one/script.js
+++ 08-dom/07-generate-table-one/script.js
@@ -9,8 +9,24 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
     // your code here
+    
+     let target = document.getElementById("target"); 
+     let tab = document.createElement("table")
+    for (let i = 0; i <= 10; i++) {
+        let row = document.createElement("tr"); 
+        for (let j = 0; j <1; j++) {
+            let column = document.createElement("td");
+            let text = document.createTextNode("Je suis le"+ i)
+            column.appendChild(text); 
+            row.appendChild(column) 
+        }
+        tab.appendChild(row)
+    }
+    target.appendChild(tab); 
+
+    
 
 })();
